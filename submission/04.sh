0dec7023e609f1257f4e8835db1d07d3527f61de
Aaron <leebaaron@outlook.com>
2026-04-14 13:36:40 -0500

    Fixed the public key hash for 4

--- submission/04.sh
+++ submission/04.sh
@@ -4,10 +4,9 @@
 timestamp=1495584032
 # conversion to little endian hex
 hex_timestamp=0x20CD2459
-# add "0x" prefix to public key
-publicKey=0x02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+# add "0x" prefix and convert to pubkeyhash by taking RIPEMD-160(SHA-256(publicKey))
+public_key_hash=0x1e51fcdc14be9a148bb0aaec9197eb47c83776fb
 
-cltv_script=$(btcc 0x20CD2459 OP_CHECKLOCKTIMEVERIFY OP_DROP OP_DUP OP_HASH160 0x02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277 OP_EQUALVERIFY OP_CHECKSIG)
+btcc 0x20CD2459 OP_CHECKLOCKTIMEVERIFY OP_DROP OP_DUP OP_HASH160 0x1e51fcdc14be9a148bb0aaec9197eb47c83776fb OP_EQUALVERIFY OP_CHECKSIG
 
-echo $cltv_script
 
